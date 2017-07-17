6d33e777617750248b3c707b24222fe067569b42
TarasOleksiv <toleksiv69@gmail.com>
2017-07-17 16:57:56 +0300

    Task4: Reorganize input process

--- src/ua/goit/java8/module4/task4/Task4.java
+++ src/ua/goit/java8/module4/task4/Task4.java
@@ -13,21 +13,27 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введіть перше ціле число: ");
-        int n = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Введіть друге ціле число: ");
-        int m = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Більшим із введених чисел " + n + " та " + m + " є число " + getMax(n,m));
 
-        System.out.print("Введіть перше довільне число: ");
-        float n1 = sc.nextFloat();
-        sc.nextLine();
-        System.out.print("Введіть друге довільне число: ");
-        float m1 = sc.nextFloat();
-        sc.nextLine();
-        System.out.println("Більшим із введених чисел " + n1 + " та " + m1 + " є число " + getMax(n1,m1));
+        System.out.print("З якими числами працюєм? (1 - цілі числа; будь-який інший символ - дробові). Введіть символ: ");
+        String c = sc.nextLine().trim();
+
+        if (c.equals("1")) {
+            System.out.print("Введіть перше ціле число: ");
+            int n = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Введіть друге ціле число: ");
+            int m = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Більшим із введених чисел " + n + " та " + m + " є число " + getMax(n,m));
+        } else {
+            System.out.print("Введіть перше дробове число: ");
+            float n1 = sc.nextFloat();
+            sc.nextLine();
+            System.out.print("Введіть друге дробове число: ");
+            float m1 = sc.nextFloat();
+            sc.nextLine();
+            System.out.println("Більшим із введених чисел " + n1 + " та " + m1 + " є число " + getMax(n1,m1));
+        }
 
     }
 
